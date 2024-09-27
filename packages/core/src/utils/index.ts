cd4356d4f39046df02e09d32810ec810b34ca9a9
yuhui.zheng <yuhui.zheng@okg.com>
2024-09-27 17:37:18 +0800

    feat: 🎸 deepclone logic optimization

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -4,14 +4,16 @@
  * @returns newObj
  */
 export function deepClone (obj: any): any {
-  if (typeof obj !== 'object') return
+  if (typeof obj !== 'object' || obj === null) return obj
+
+  // not clone instance of class
+  if (obj.constructor && obj.constructor !== Object && !(obj instanceof Array)) {
+    return obj
+  }
+
   const newObj: any = obj instanceof Array ? [] : {}
   for (const key in obj) {
-    if (typeof obj[key] === 'object') {
-      newObj[key] = deepClone(obj[key])
-    } else {
-      newObj[key] = obj[key]
-    }
+    newObj[key] = deepClone(obj[key])
   }
   return newObj
 }
